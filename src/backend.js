c497e71ba5e39e1466851a6bc33855492d219eba
Mike <mmcshane10@gmail.com>
2019-09-13 14:44:58 -0700

    add row to include header and new images

--- src/backend.js
+++ src/backend.js
@@ -2,7 +2,7 @@ export class DoctorSearch {
   getDoctorResults(keyword, name) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${keyword}&location=or-portland&limit=50&user_key=${process.env.exports.apiKey}`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${keyword}&location=or-portland&limit=500&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -22,52 +22,54 @@ $(document).ready(function() {
       let newPatients;
       if (body.data.length === 0) {
         $("#search-results").append(`<div class='strong'>Sorry, your search did not return any results. Please try searching with different parameters.</div>`);
-      }
-      for (let i = 0; i < body.data.length; i++) {
-        $("#search-results").append(
-          `<div id="accordion">
-          <div class="card bg-light mb-3">
-          <div class="card-header" id="heading${i}" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapseOne"><span id=name${i}></div>
-          <div id="collapse${i}" class="collapse show" aria-labelledby="heading${i}" data-parent="#accordion">
-          <div class="card-body">
-          <div id=image${i} class="image"></div>
-          <div id=specialty${i}></div>
-          <div id=practice${i}></div>
-          <div id=address${i}></div>
-          <div id=phone${i}></div>
-          <div id=newpatients${i}></div>
-          <div id=profile${i}></div>
-          </div>
-          </div>
-          </div>
-          </div>`);
+      } else {
+        for (let i = 0; i < body.data.length; i++) {
+          $("#search-results").append(
+            `<div id="accordion">
+            <div class="card bg-light mb-3">
+            <div class="card-header" id="heading${i}" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapseOne"><span id=name${i}></div>
+            <div id="collapse${i}" class="collapse show" aria-labelledby="heading${i}" data-parent="#accordion">
+            <div class="card-body">
+            <div id=image${i} class="image"></div>
+            <div id=specialty${i}></div>
+            <div id=practice${i}></div>
+            <div id=address${i}></div>
+            <div id=phone${i}></div>
+            <div id=newpatients${i}></div>
+            <div id=profile${i}></div>
+            </div>
+            </div>
+            </div>
+            </div>`);
 
-          if (body.data[i].practices[0].accepts_new_patients === true) {
-            newPatients = "Yes"
-          } else {
-            newPatients = "No"
+
+            $(`#name${i}`).html(`${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, ${body.data[i].profile.title}`);
+
+            $(`#image${i}`).html(`<img class="card-img-left" src=${body.data[i].profile.image_url} alt="Card image cap">`)
+
+            $(`#practice${i}`).html(`<p><span class='strong'>Practice Name:</span> ${body.data[i].practices[0].name}</p>`)
+
+            $(`#specialty${i}`).html(`<p><span class='strong'>Specialty:</span> ${body.data[i].specialties[0].description}</p>`)
+
+
+            $(`#address${i}`).html(`<p><span class='strong'>Address:</span> ${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}, ${body.data[i].practices[0].visit_address.zip}`);
+
+            $(`#phone${i}`).html(`<p><span class='strong'>Phone:</span> ${body.data[i].practices[0].phones[0].number}</p>`);
+
+            if (body.data[i].practices[0].accepts_new_patients === true) {
+              newPatients = "Yes"
+            } else {
+              newPatients = "No"
+            }
+
+            $(`#newpatients${i}`).html(`<p><span class='strong'>Accepting New Patients:</span> ${newPatients} </p>`);
+
+            $(`#profile${i}`).html(`<p>${body.data[i].profile.bio}</p>`)
           }
-
-          $(`#name${i}`).html(`${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, ${body.data[i].profile.title}`);
-
-          $(`#image${i}`).html(`<img class="card-img-left" src=${body.data[i].profile.image_url} alt="Card image cap">`)
-
-          $(`#practice${i}`).html(`<p><span class='strong'>Practice Name:</span> ${body.data[i].practices[0].name}</p>`)
-
-          $(`#specialty${i}`).html(`<p><span class='strong'>Specialty:</span> ${body.data[i].specialties[0].description}</p>`)
-
-
-          $(`#address${i}`).html(`<p><span class='strong'>Address:</span> ${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}, ${body.data[i].practices[0].visit_address.zip}`);
-
-          $(`#phone${i}`).html(`<p><span class='strong'>Phone:</span> ${body.data[i].practices[0].phones[0].number}</p>`);
-
-          $(`#newpatients${i}`).html(`<p><span class='strong'>Accepting New Patients:</span> ${newPatients} </p>`);
-
-          $(`#profile${i}`).html(`<p>${body.data[i].profile.bio}</p>`)
         }
 
       }, function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+        $('#show-errors').text(`There was an error processing your request: ${error.message}`);
       });
     });
   });
