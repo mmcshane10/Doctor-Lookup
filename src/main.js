d4f802c844417ed86e6c7141ca2972c10b5100d6
Mike <mmcshane10@gmail.com>
2019-09-13 09:45:09 -0700

    main js now creates a new empty card and div for each doctor found in the search result.

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
     let name = $('#name-search').val();
     $('#keyword-search').val("");
     $('#name-search').val("");
+    $('#search-results').val("");
 
 
     let doctorSearch = new DoctorSearch();
@@ -18,7 +19,26 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       const body = JSON.parse(response);
-      console.log(body);
+      console.log(body.meta.total);
+      for (let i = 0; i < body.meta.total; i++) {
+        $("#search-results").append(
+          `<div id="accordion">
+          <div class="card bg-light mb-3">
+          <div class="card-header" id="heading${i}" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapseOne"><span id=title${i}></div>
+          <div id="collapse${i}" class="collapse show" aria-labelledby="heading${i}" data-parent="#accordion">
+          <div class="card-body">
+          <div id=company${i}></div>
+          <div id=location${i}></div>
+          <div id=description${i}></div>
+          <div id=url${i}></div>
+          </div>
+          </div>
+          </div>
+          </div>`);
+      }
+
+
+
 
     }, function(error) {
 
